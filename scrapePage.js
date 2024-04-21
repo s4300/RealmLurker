4365b114ffe66fcbce8154b9a05e96be46612d62
Spinn0 <93995622+s4300@users.noreply.github.com>
2024-04-21 07:17:19 -0700

    Removed duplicates from realm list + Fixed scraper

--- scrapePage.js
+++ scrapePage.js
@@ -2,7 +2,7 @@ let links = document.querySelectorAll("a");
 let spans = document.querySelectorAll("span");
 let codesFound = [];
 
-let existingJson = prompt("Insert existing code list json (If there isn't any, just leave this blank!)");
+let existingJson = prompt("Insert existing code list json. (If there isn't any, just leave this blank!)");
 try {
     if (JSON.parse(existingJson)) {
         codesFound = JSON.parse(existingJson);
@@ -13,23 +13,17 @@ try {
 
 for (const link of links) {
     let href = link.href;
-    if (href.startsWith("https://realms.gg/") && codesFound.includes(href) !== true) {
+    if (href.startsWith("https://realms.gg/")) {
+        // Extracts the actual code from the link
         let code = href.replace("https://realms.gg/", "");
         if (code.startsWith("/")) {
             code = code.replace("/", "");
         };
+        // Check if code is already in the list, if so, then skip this code.
+        if (codesFound.includes(code)) break;
         //codesFound.push({"code": code});
         codesFound.push(code);
     };
 };
-// for (const span of spans) {
-//     let text = span.textContent.toLowerCase();
-//     if (text.startsWith("realm code: ") && codesFound.includes(text) !== true) {
-//         codesFound.push(text);
-//     };
-//     if (text.startsWith("code: ") && codesFound.includes(text) !== true) {
-//         codesFound.push(text);
-//     };
-// };
 
 console.log(`New JSON (Paste into realmcodes.json file!): ${JSON.stringify(codesFound)}`)
