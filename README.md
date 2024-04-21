8fd1b19bac7df573eb7c432a0bd3c1ddfc3f6d7d
Spinn0 <93995622+s4300@users.noreply.github.com>
2024-04-20 18:56:41 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# RealmLurker
+Easily find public realms.
+
+## Info
+[Discord](https://discord.gg/Heevz7vYNW)
+
+## Assistance
+> How can I use this tool?
+
+You can use RealmLurker [here](https://s4300.github.io/realmlurker).
+
+> How can I contribute?
+
+You can help by adding to our realm list, or improving site functionality/design.
---
+++ scrapePage.js
@@ -0,0 +1,35 @@
+let links = document.querySelectorAll("a");
+let spans = document.querySelectorAll("span");
+let codesFound = [];
+
+let existingJson = prompt("Insert existing code list json (If there isn't any, just leave this blank!)");
+try {
+    if (JSON.parse(existingJson)) {
+        codesFound = JSON.parse(existingJson);
+    };
+} catch {
+    console.log("Error loading JSON.");
+};
+
+for (const link of links) {
+    let href = link.href;
+    if (href.startsWith("https://realms.gg/") && codesFound.includes(href) !== true) {
+        let code = href.replace("https://realms.gg/", "");
+        if (code.startsWith("/")) {
+            code = code.replace("/", "");
+        };
+        //codesFound.push({"code": code});
+        codesFound.push(code);
+    };
+};
+// for (const span of spans) {
+//     let text = span.textContent.toLowerCase();
+//     if (text.startsWith("realm code: ") && codesFound.includes(text) !== true) {
+//         codesFound.push(text);
+//     };
+//     if (text.startsWith("code: ") && codesFound.includes(text) !== true) {
+//         codesFound.push(text);
+//     };
+// };
+
+console.log(`Found links: ${JSON.stringify(codesFound)}`)
